96b83fa9cd11982fb5f92dc41d436859826cabf2
nortonnick19 <norton.nick19@gmail.com>
2026-08-18 14:13:17 +0300

    main.py

--- control-server/app/main.py
+++ control-server/app/main.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
 
+
+app = FastAPI(
+    title="TG Access Control API",
+    version="0.1"
+)
+
+
+@app.get("/")
+async def root():
+    return {
+        "status": "online",
+        "service": "control-server"
+    }
