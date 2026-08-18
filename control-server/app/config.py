31a6bda4b5e7c0c4ec44a36d17f6839755e9cb6e
nortonnick19 <norton.nick19@gmail.com>
2026-08-18 14:12:53 +0300

    config.py

--- control-server/app/config.py
+++ control-server/app/config.py
@@ -1,1 +1,17 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+asyncpg://localhost/accessbot"
+)
+
+
+ADMIN_IDS = [
+    123456789
+]
